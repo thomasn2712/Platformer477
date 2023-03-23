c5b61d949692d3f6625ab12b28dea1134e723bc6
thomasn2712 <112751375+thomasn2712@users.noreply.github.com>
2023-03-23 18:09:42 -0500

    Player can move sprites by pressing F

--- Crawl Out the Fallout/Assets/Scripts/Player.cs
+++ Crawl Out the Fallout/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private float speed;
+    public GameObject couch;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,24 +15,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            // Movement 
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.Translate(Vector3.up * speed * Time.deltaTime);
+        float playerCouchDistance = Mathf.Abs(couch.transform.position.x - transform.position.x);
+        float playerCouchHeightDistance = Mathf.Abs(couch.transform.position.y - transform.position.y);
+        if ((playerCouchDistance < 2) || (playerCouchHeightDistance < 0.5)) {
+            if (Input.GetKey(KeyCode.F)) {
+                couch.transform.position = new Vector3(transform.position.x, couch.transform.position.y, couch.transform.position.z);
             }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.Translate(-Vector3.up * speed * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.Translate(Vector3.right * speed * Time.deltaTime);
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.Translate(-Vector3.right * speed * Time.deltaTime);
-            }
-
-        
+        }
+        // Movement 
+        if ((Input.GetKey(KeyCode.UpArrow)) || (Input.GetKey(KeyCode.W)))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        else if ((Input.GetKey(KeyCode.DownArrow)) || (Input.GetKey(KeyCode.S)))
+        {
+            transform.Translate(-Vector3.up * speed * Time.deltaTime);
+        }
+        else if ((Input.GetKey(KeyCode.RightArrow)) || (Input.GetKey(KeyCode.D)))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        else if ((Input.GetKey(KeyCode.LeftArrow)) || (Input.GetKey(KeyCode.A)))
+        {
+            transform.Translate(-Vector3.right * speed * Time.deltaTime);
+        }
     }
 }
